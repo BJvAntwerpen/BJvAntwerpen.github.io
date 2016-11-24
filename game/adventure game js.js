4c6089abca260f5bd4c11befb4833de822c9c303
Bailey van Antwerpen <99041848@mydavinci.nl>
2016-11-24 11:17:20 +0100

    test for cookies

--- game/adventure game js.js
+++ game/adventure game js.js
@@ -29,6 +29,41 @@ var actions = {
 	safeOpened: false
 }
 
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*60*60*1000));
+    var expires = "expires=" + d.toUTCString();
+    console.log(expires);
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 1.05);
+       }
+    }
+}
+
 function safeCode() {
 	var i;
 	for (i = 0; i < 4; i++) {
