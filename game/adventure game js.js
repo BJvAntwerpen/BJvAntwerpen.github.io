010622e92ccb0c83a37a3f817478632d334369e8
Bailey van Antwerpen <99041848@mydavinci.nl>
2016-12-07 10:24:50 +0100

    update

--- game/adventure game js.js
+++ game/adventure game js.js
@@ -36,42 +36,7 @@ var Storyline = {
 	Beach: false,
 	Volcano: false
 }
-/*
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*60*1000));
-    var expires = "expires=" + d.toUTCString();
-    console.log(expires);
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","This is just a test");
-       if (user != "" && user != null) {
-           setCookie("username", user, 1.05);
-       }
-    }
-}
-*/
 function safeCode() {
 	var i;
 	for (i = 0; i < 4; i++) {
